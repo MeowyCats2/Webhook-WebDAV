ff7152cd46580e9acb569d362dcf90f40791fbf6
MeowyCats2 <meowycats2@protonmail.com>
2024-04-12 04:52:46 +0000

    Attempt to fix moving entries

--- webhook.js
+++ webhook.js
@@ -192,7 +192,7 @@ export const messageFromPath = async (path) => {
 }
 export const moveEntry = async (entry, fromId, toId) => {
   const fromData = await getEntry(fromId)
-  fromData.contents.splice(fromData.contents.find(c => entry.id === c.metadata), 1)
+  fromData.contents.splice(fromData.contents.findIndex(c => entry.id === c.metadata), 1)
   await edit_msg(fromId, new Blob([JSON.stringify(fromData)]), "file.json")
   setCache(fromId, fromData)
   await appendToFolder(entry.type, entry.id, toId, entry.name)
